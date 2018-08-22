7243422fec185e536f579dc1443b2913479a0b7a
Travis CI User <travis@example.org>
2018-08-22 20:15:20 +0000

    Update docs after building Travis build 26 of mattjshannon/swsnet
    The docs were built from the branch 'master' against the commit
    685744aa6f7a45c27d6b0cbae77314fcef2964c0.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/mattjshannon/swsnet/jobs/419352770.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post36+g2440463',
+    VERSION: '0.post39+g685744a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
