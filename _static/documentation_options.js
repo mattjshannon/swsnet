78575d49609945392206970e868c400090f3260e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-10-23 23:51:31 +0000

    Update docs after building Travis build 60 of mattjshannon/swsnet
    The docs were built from the branch 'master' against the commit
    1f50e9c504988ca9a532b0fe0484814ee62ffb52.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/mattjshannon/swsnet/jobs/445398988.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post62+g9d03b42',
+    VERSION: '0.post66+g1f50e9c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
