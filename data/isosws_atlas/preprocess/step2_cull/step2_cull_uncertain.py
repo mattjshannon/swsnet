556b422530e988b834010cf4c71c1de6bf4bfd46
Matt Shannon <matthew.j.shannon@gmail.com>
2018-11-20 16:45:01 -0800

    Added manual-flagging to culled spectra list

--- data/isosws_atlas/preprocess/step2_cull/step2_cull_uncertain.py
+++ data/isosws_atlas/preprocess/step2_cull/step2_cull_uncertain.py
@@ -1,6 +1,8 @@
 
 # coding: utf-8
 
+# # Remove untrustworthy sources
+
 # In[1]:
 
 
@@ -26,52 +28,102 @@ meta
 # 
 # First let's cull based on the uncertainty flag, and if it's group 7. i.e., for those data, let's set the ``data_ok`` flag to ``False``.
 
+# ### Manual quality flags
+
+# In[ ]:
+
+
+def apply_manual_flags(df):
+    return df
+
+
+# In[12]:
+
+
+tdts, flags = np.loadtxt('quality_flags.txt', delimiter=',').T.astype(int)
+
+
+# In[15]:
+
+
+flags
+
+
+# In[24]:
+
+
+plt.hist(flags, width=0.5)
+plt.xlim(right=2.5)
+
+
 # ### Exclude group 7 and uncertain data
 
-# In[6]:
+# In[34]:
 
 
 updated_data_ok = []
 
 for row in meta.itertuples():
     index = row[0]
+
+    # Attributes needed for determining quality of spectrum.
+    tdt = getattr(row, "tdt")
     group_num = getattr(row, "group")
+    flag = getattr(row, "uncertainty_flag")
+    
+    # Don't trust group 7, or anything with a flag.
     if (int(group_num) == 7) or (flag != ''):
-        updated_data_ok.append(False)
+        trust_tdt = False
     else:
-        updated_data_ok.append(True)
+        trust_tdt = True
+        
+    # Don't trust manually flagged spectra.
+    tdt_index = np.where(tdts == tdt)[0][0]
+    manual_flag = flags[tdt_index]
+    if manual_flag == 0:
+        trust_tdt = True
+    else:
+        trust_tdt = False        
+
+    updated_data_ok.append(trust_tdt)
         
 print('Number still OK: ', sum(updated_data_ok))
 
 
 # ### Save to disk as a new pickle file
 
-# In[7]:
+# In[35]:
 
 
 meta2 = meta.copy()
 
 
-# In[8]:
+# In[36]:
 
 
 meta2.head()
 
 
-# In[9]:
+# In[37]:
 
 
 meta2['data_ok'] = updated_data_ok
 
 
-# In[10]:
+# In[38]:
 
 
 meta2
 
 
-# In[12]:
+# In[39]:
 
 
 meta2.to_pickle('../metadata_step2_culled.pkl')
+
+
+# In[41]:
+
+
+np.sum(meta2['data_ok'].values)
 
